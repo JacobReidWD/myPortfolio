b97972f00a812018dabb8946ec33c8bd84a5c84c
Jacob Reid <72713602+JacobReidWD@users.noreply.github.com>
2020-10-11 23:07:06 -0500

    updated the js file for the main index page.

--- js/Script.js
+++ js/Script.js
@@ -1,37 +1,31 @@
 function changeColor(obj) {
-    
-    if (obj.style.color == 'black') {
-        obj.style.color = 'black';
-        
-    } else {
-        obj.style.color = '#13FFFA';
-    }
+  if (obj.style.color == "black") {
+    obj.style.color = "black";
+  } else {
+    obj.style.color = "#13FFFA";
+  }
 }
 
 function changeBgColor(obj, colorName) {
-    obj.style.backgroundColor = colorName;
-    
+  obj.style.backgroundColor = colorName;
 }
 
 function changeColorUI(obj, colorName) {
-    
-    if (obj.style.color == 'black') {
-        obj.style.color = 'black';
-        
-    } else {
-        obj.style.color = colorName;
-    }
+  if (obj.style.color == "black") {
+    obj.style.color = "colorName";
+  } else {
+    obj.style.color = colorName;
+  }
 }
 
 function changeSize(obj, sizer) {
-    obj.style.fontSize = sizer;
-    
+  obj.style.fontSize = sizer;
 }
 
 function changeShadow(obj, shader) {
-    obj.style.boxShadow = shader;
+  obj.style.boxShadow = shader;
 }
 
 function changePos(obj, pos) {
-    obj.style.Bottom = pos;
+  obj.style.Bottom = pos;
 }
